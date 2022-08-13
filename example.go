5acb23d3d6395282b1224f86c40598db6a390793
Lex <alex@bighill.org>
2022-08-13 13:42:17 -0700

    Handle POST data.

--- example.go
+++ example.go
@@ -14,6 +14,11 @@ type CheckboxField struct {
 	Colors []string `form:"color_field"`
 }
 
+type JsonPostData struct {
+	Star   string `form:"star"`
+	Planet string `form:"planet"`
+}
+
 func GetFormField(c *gin.Context) {
 	var f FormField
 	c.Bind(&f)
@@ -24,8 +29,15 @@ func GetFormField(c *gin.Context) {
 func GetCheckboxField(c *gin.Context) {
 	var checks CheckboxField
 	c.ShouldBind(&checks)
-	fmt.Printf("check boxes: %s\n", checks.Colors)
+	fmt.Printf("check boxes: %s \n", checks.Colors)
 	c.JSON(200, gin.H{"check_boxes": checks.Colors})
+}
+
+func PostData(c *gin.Context) {
+	var data JsonPostData
+	c.Bind(&data)
+	fmt.Printf("post data: Star is %s, Planet is %s \n", data.Star, data.Planet)
+	c.JSON(200, gin.H{"star": data.Star, "planet": data.Planet})
 }
 
 func main() {
@@ -42,6 +54,9 @@ func main() {
 
 	// curl "localhost:8080/form-colors?color_field=red&color_field=blue"
 	r.GET("/form-colors", GetCheckboxField)
+
+	// curl -X POST localhost:8080/post-data --data '{"star":"Sun","planet":"Venus"}' -H "Content-Type:application/json"
+	r.POST("/post-data", PostData)
 
 	r.Run()
 }
