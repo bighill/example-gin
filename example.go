7056549bbe3d3ca44b96ec3cf87ab9649aee6aca
Lex <alex@bighill.org>
2022-08-13 14:04:39 -0700

    Handle dynamic URI routes.

--- example.go
+++ example.go
@@ -6,17 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+/*
+form
+*/
 type FormField struct {
 	Apple string `form:"apple_field"`
-}
-
-type CheckboxField struct {
-	Colors []string `form:"color_field"`
-}
-
-type JsonPostData struct {
-	Star   string `form:"star"`
-	Planet string `form:"planet"`
 }
 
 func GetFormField(c *gin.Context) {
@@ -26,6 +20,13 @@ func GetFormField(c *gin.Context) {
 	c.JSON(200, gin.H{"apple_message": f.Apple})
 }
 
+/*
+checkbox
+*/
+type CheckboxField struct {
+	Colors []string `form:"color_field"`
+}
+
 func GetCheckboxField(c *gin.Context) {
 	var checks CheckboxField
 	c.ShouldBind(&checks)
@@ -33,11 +34,39 @@ func GetCheckboxField(c *gin.Context) {
 	c.JSON(200, gin.H{"check_boxes": checks.Colors})
 }
 
+/*
+post
+*/
+type JsonPostData struct {
+	Star   string `form:"star"`
+	Planet string `form:"planet"`
+}
+
 func PostData(c *gin.Context) {
 	var data JsonPostData
 	c.Bind(&data)
-	fmt.Printf("post data: Star is %s, Planet is %s \n", data.Star, data.Planet)
 	c.JSON(200, gin.H{"star": data.Star, "planet": data.Planet})
+	fmt.Printf("post data: Star is %s, Planet is %s \n", data.Star, data.Planet)
+}
+
+/*
+dynamic URI
+*/
+type DynamicUri struct {
+	ID   string `uri:"id" binding:"required,uuid"`
+	Name string `uri:"name" binding:"required"`
+}
+
+func GetDynamicUri(c *gin.Context) {
+	var uri DynamicUri
+	err := c.ShouldBindUri(&uri)
+	if err != nil {
+		c.JSON(400, gin.H{"message": "invalid URI", "error": err})
+		fmt.Println("invalid URI")
+		return
+	}
+	c.JSON(200, gin.H{"name": uri.Name, "uuid": uri.ID})
+	fmt.Printf("dynamic URI: Name is %s, UUID is %s \n", uri.Name, uri.ID)
 }
 
 func main() {
@@ -57,6 +86,10 @@ func main() {
 
 	// curl -X POST localhost:8080/post-data --data '{"star":"Sun","planet":"Venus"}' -H "Content-Type:application/json"
 	r.POST("/post-data", PostData)
+
+	// curl -v localhost:8080/dynamic-uri/lex/brokenuuid
+	// curl localhost:8080/dynamic-uri/lex/987fbc97-4bed-5078-9f07-9141ba07c9f3
+	r.GET("/dynamic-uri/:name/:id", GetDynamicUri)
 
 	r.Run()
 }
