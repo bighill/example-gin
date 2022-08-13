ef80f4d5c659b161dd449a23b88d425814e46408
Lex <alex@bighill.org>
2022-08-13 13:16:53 -0700

    Handle GET params.

--- example.go
+++ example.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
+
+type FormField struct {
+	Apple string `form:"apple_field"`
+}
+
+func GetFormField(c *gin.Context) {
+	var f FormField
+	c.Bind(&f)
+	fmt.Printf("apple message: %s\n", f.Apple)
+	c.JSON(200, gin.H{"apple_message": f.Apple})
+}
 
 func main() {
 	r := gin.Default()
@@ -12,6 +25,9 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	// curl "localhost:8080/form?apple_field=Apples_are_for_eating"
+	r.GET("/form", GetFormField)
 
 	r.Run()
 }
