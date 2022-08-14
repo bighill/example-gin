4524ee66876a1304c657acb75915071e10ac4bee
Lex <alex@bighill.org>
2022-08-13 19:10:45 -0700

    Cleanup.

--- template.go
+++ template.go
@@ -4,7 +4,17 @@ import "github.com/gin-contrib/multitemplate"
 
 func initRenderer() multitemplate.Renderer {
 	r := multitemplate.NewRenderer()
-	r.AddFromFiles("default", "templates/default.html", "templates/header.html", "templates/footer.html")
-	r.AddFromFiles("secondary", "templates/secondary.html", "templates/header.html", "templates/footer.html")
+	r.AddFromFiles(
+		"default",
+		"templates/default.html",
+		"templates/header.html",
+		"templates/footer.html",
+	)
+	r.AddFromFiles(
+		"secondary",
+		"templates/secondary.html",
+		"templates/header.html",
+		"templates/footer.html",
+	)
 	return r
 }
